d7918d896443fef39080d49694dcf591d33ebe11
stefjen <stefjen@yandex-team.ru>
2024-12-03 10:23:35 +0500

    Initial Commit

---
+++ YandexCupFinals2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YandexCupFinals2024
+//
+//  Created by Yauheni Stsefankou on 03.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YandexCupFinals2024/YandexCupFinals2024App.swift
@@ -0,0 +1,17 @@
+//
+//  YandexCupFinals2024App.swift
+//  YandexCupFinals2024
+//
+//  Created by Yauheni Stsefankou on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct YandexCupFinals2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
